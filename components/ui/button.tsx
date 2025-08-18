38fea9df9f57e83582326c1e498523b52d1b05d8
oliverrr123 <oliver.cingl@gmail.com>
2025-08-18 13:21:59 -0700

    fix

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "wave-primary bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "wave-primary bg-primary text-primary-foreground hover:bg-primary/90 border-0",
         destructive:
           "wave-primary bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
@@ -19,7 +19,7 @@ const buttonVariants = cva(
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
         wave: "wave-secondary bg-[#EEE] text-[#163F69] hover:text-[#163F69]",
-        wavePrimary: "wave-primary bg-[#163F69] text-white hover:text-white border border-transparent",
+        wavePrimary: "wave-primary bg-[#163F69] text-white hover:text-white border-0",
       },
       size: {
         default: "h-10 px-4 py-2",
