4d66fbf8af888d5a7cca2daca59fd6a77c2419a4
oliverrr123 <oliver.cingl@gmail.com>
2025-08-30 03:15:29 -0700

    fix

--- components/LoginModal.tsx
+++ components/LoginModal.tsx
@@ -37,7 +37,7 @@ export function LoginModal({ open, onOpenChange, onSwitchToSignup }: LoginModalP
     <Dialog open={open} onOpenChange={onOpenChange}>
       <DialogPortal>
         <DialogOverlay className="fixed inset-0 z-50 bg-black/50 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0" />
-        <DialogContent className="fixed left-[50%] top-[50%] z-50 w-full max-w-md translate-x-[-50%] translate-y-[-50%] bg-white rounded-3xl shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] border-0 p-0">
+        <DialogContent className="fixed left-[50%] top-[5vh] z-50 w-full max-w-md translate-x-[-50%] bg-white rounded-3xl shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] border-0 p-0 max-h-[90vh] overflow-y-auto sm:top-[50%] sm:translate-y-[-50%]">
           <div className="relative p-8">
             {/* Close button */}
             <button
